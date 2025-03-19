0f053efc9a158a9f0ea59034bc0d7035cad70080
MoustafaDarweeshDev <moustafa.darweesh.dev@gmail.com>
2025-03-19 09:18:47 +0200

    updated url column

--- CleanArch/CleanArch.Domain/Models/Course.cs
+++ CleanArch/CleanArch.Domain/Models/Course.cs
@@ -13,6 +13,6 @@ namespace CleanArch.Domain.Models
         public int ld {get; set;}
         public string Name { get; set; }
         public string Description { get; set; }
-        public string ImageUr1 { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
---
+++ CleanArch/CleanArch.Infra.Data/Migrations/20250319071727_RenameImageUr1ToImageUrl.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanArch.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameImageUr1ToImageUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageUr1",
+                table: "Courses",
+                newName: "ImageUrl");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Courses",
+                newName: "ImageUr1");
+        }
+    }
+}
